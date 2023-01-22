76f0cfe98c12b9b128dbd6dfcde25ad5ffcbf75a
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-01-22 23:31:09 +0600

    Fist task in HW1 completed

---
+++ HW1-Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.WriteLine("Введите число A");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int numberB = int.Parse(Console.ReadLine());
+
+int i = numberA/numberB;
+
+if (i == numberB)
+{
+    Console.WriteLine("да, первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("нет, первое число не является квадратом второго");
+}
