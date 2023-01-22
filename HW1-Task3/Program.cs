98254378a99c105d527e2d9d8c1ccee00e64b185
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-01-23 00:43:34 +0600

    Task3 in HW1 completed

---
+++ HW1-Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Выяснить является ли число чётным
+
+int N = new Random().Next(1,100);
+Console.WriteLine(N);
+
+int i = N/2;
+Console.WriteLine("N/2= " + i);
+
+int x = N-i;
+Console.WriteLine("x= " + x);
+
+if(i == x)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
