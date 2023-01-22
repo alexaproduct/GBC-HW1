0d094b1f63b0b09642a1df1a8083272d917c805e
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-01-23 01:04:26 +0600

    Task4 in HW1 completed

---
+++ HW1-Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать четные числа от 1 до N
+
+int N = new Random().Next(1,100);
+Console.WriteLine("N= " + N);
+
+int b = 1+1;
+
+while(b<=N)
+{
+    Console.WriteLine(b);
+    b = b + 2;
+}
+
+
+
