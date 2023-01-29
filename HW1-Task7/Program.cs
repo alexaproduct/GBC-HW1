29073b5b825c049a1fb58e996282c0186ade4181
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-01-29 20:30:25 +0600

    Task7 in HW1 completed

---
+++ HW1-Task7/Program.cs
@@ -0,0 +1,26 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int N = new Random().Next(10,99);
+Console.WriteLine(N);
+
+int a = N/10;
+Console.WriteLine("a=" + a);
+
+int b = N-a*10;
+Console.WriteLine("b=" + b);
+
+if(a > b)
+{
+    Console.WriteLine("a = макс");
+}
+else
+{
+    if(b > a)
+    {
+        Console.WriteLine("b = макс");
+    }
+    else
+    {
+         Console.WriteLine("числа равны");
+    }
+}
