def8d043b93de2d6e2036ca6ed41606a74588f58
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-01-29 20:36:30 +0600

    Task8 in HW1 completed

---
+++ HW1-Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int N = new Random().Next(99,1000);
+Console.WriteLine(N);
+
+int a = N/100;
+Console.WriteLine(a);
+
+int b = (N-a*100)/10;
+Console.WriteLine(b);
+
+int c = N-a*100-b*10;
+Console.WriteLine(c);
+
+int N2 = a*10+c;
+Console.WriteLine(N2);
