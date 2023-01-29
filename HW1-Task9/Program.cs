7ca95bcb1d9bca525900b9dd26f0e32fc8d324e7
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-01-29 20:54:26 +0600

    Task9 in HW1 completed

---
+++ HW1-Task9/Program.cs
@@ -0,0 +1,21 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int X = 12;
+Console.WriteLine("X = " + X);
+
+int Y = 5;
+Console.WriteLine("Y = " + Y);
+
+int Z = X/Y;
+
+int A = X-Z*Y;
+Console.WriteLine("Остаток = " + A);
+
+if (A == 0)
+{
+   Console.WriteLine("X и Y кратны, частное = " + Z);
+}
+else
+{
+    Console.WriteLine("X и Y не кратны, остаток = " + A);
+}
