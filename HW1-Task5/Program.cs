ac50f958e04a37ecf6ecc91d5dd54c41e27116c8
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-01-29 20:19:31 +0600

    Task 5 in HW1 completed

---
+++ HW1-Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int N = new Random().Next(1,1000);
+Console.WriteLine(N);
+
+int a = N/100;
+
+int b = (N-a*100)/10;
+
+int c = N-a*100-b*10;
+Console.WriteLine(c);
