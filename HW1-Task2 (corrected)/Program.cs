9bcae5c05d44136fc34b25cd2d3feca15c98c925
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-04 16:25:38 +0600

    Исправленное решение задачи 2

---
+++ HW1-Task2 (corrected)/Program.cs
@@ -0,0 +1,12 @@
+﻿// // По заданному номеру дня недели вывести его название
+
+Console.WriteLine("Введите номер дня недели:");
+int i = int.Parse(Console.ReadLine());
+
+if (i==1) Console.WriteLine("Понедельник");
+if (i==2) Console.WriteLine("Вторник");
+if (i==3) Console.WriteLine("Среда");
+if (i==4) Console.WriteLine("Четверг");
+if (i==5) Console.WriteLine("Пятница");
+if (i==6) Console.WriteLine("Суббота");
+if (i==7) Console.WriteLine("Воскресенье");
