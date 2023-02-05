c0cb72c701b24c4eaae624f83968a89541035e30
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-05 19:55:03 +0600

    Corrected Task3 added

---
+++ HW1-Task3 (corrected)/Program.cs
@@ -0,0 +1,7 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Введите число:");
+int i = int.Parse(Console.ReadLine());
+
+if (i % 2 == 0) Console.WriteLine("Четное");
+else Console.WriteLine("Нечетное");
