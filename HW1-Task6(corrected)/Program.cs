f559d3e424c975abd90448d65208a841356e9425
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-05 20:26:38 +0600

    Task 6 corrected added

---
+++ HW1-Task6(corrected)/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Первый вариант решения");
+
+Console.WriteLine("Введите 3-значное число:");
+int i = int.Parse(Console.ReadLine());
+
+
+if (i/1000 > 1 || i/100 < 1) Console.WriteLine("Не является 3-значным числом. Введите 3-значное число.");
+
+int k = (i%100-i%10)/10;
+Console.WriteLine("2-я цифра = "+k);
+
+// Второй вариант решения - через string
+
+Console.WriteLine("Второй вариант решения - через String");
+
+string stringNumber = Convert.ToString(i);
+Console.WriteLine("2-я цифра 3-значного числа -> "+stringNumber[1]);
