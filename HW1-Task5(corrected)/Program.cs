f66806ecc1d3ba4769eb178222b2506ca6464770
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-05 20:08:38 +0600

    Task5 corrected added

---
+++ HW1-Task5(corrected)/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите 3-значное число:");
+int i = int.Parse(Console.ReadLine());
+
+
+if (i/1000 > 1 || i/100 < 1) Console.WriteLine("Не является 3-значным числом. Введите 3-значное число.");
+else if (i/100 > 1)
+{
+    int k = i%10;
+    Console.WriteLine(k);
+}
+
