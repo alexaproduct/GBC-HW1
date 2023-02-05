d3ef7f7269126a713bbd59375d38a948831b710e
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-05 21:02:37 +0600

    Task 9 corrected added

---
+++ HW1-Task9(corrected)/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int A = 100;
+Console.WriteLine("Задано число A = "+A);
+
+Console.WriteLine("Введите число B:");
+int B = int.Parse(Console.ReadLine());
+
+int ostatok1 = A%B;
+
+if (A%B == 0) Console.WriteLine("кратно");
+else Console.WriteLine(ostatok1);
+
