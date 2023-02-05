07e0e78423f41fe2ecccac204c789aeb29e9e740
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-05 21:13:13 +0600

    Task 10 corrected added

---
+++ HW1-Task10(corrected)/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("Введите число:");
+int i = int.Parse(Console.ReadLine());
+
+if (i/100 < 1) 
+{
+    Console.WriteLine("Нет 3-й цифры в числе. Введите число из 3-х или более цифр.");
+}
+else
+{
+    string stringNumber = Convert.ToString(i);
+    Console.WriteLine("третья цифра этого числа -> "+stringNumber[2]);
+}
